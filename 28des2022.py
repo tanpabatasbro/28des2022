a851cce07716eea0ed6fea78b5ea54f1a930639c
tanpabatasbro <115479649+tanpabatasbro@users.noreply.github.com>
2022-12-28 20:55:16 +0800

    Add files via upload

---
+++ 28des2022.py
@@ -0,0 +1,10 @@
+a = int(input("Masukkan angka : "))
+
+if a%5==0 and a%3==0:
+    print("Master Class")
+elif a%5==0:
+    print("Pride Of 5")
+elif a%3==0:
+    print("Pride Of 3")
+else:
+    print("ERRORRRR")
